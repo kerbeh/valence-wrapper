726569fd821cf74da51fa93d292f94c5e98e1531
Tristan Kirby <t.kirby@deakin.edu.au>
2018-12-18 09:01:31 +1100

    🔥 Fixed namespaces

--- src/GuzzleAdapter.php
+++ src/GuzzleAdapter.php
@@ -1,48 +1,60 @@
 <?php
+namespace ValenceWrapper;
 
-class GuzzleAdapter implements HttpClient {
+class GuzzleAdapter implements HttpClient
+{
 
     protected $protocol;
     protected $host;
     protected $port;
 
-    public function get($url) {
+    public function get($url)
+    {
 
     }
 
-    public function concurrentGet($urlArray) {
+    public function concurrentGet($urlArray)
+    {
 
     }
 
-    public function post($url, $json) {
+    public function post($url, $json)
+    {
 
     }
 
-    public function put($url, $json) {
+    public function put($url, $json)
+    {
 
     }
 
-    public function delete($url) {
+    public function delete($url)
+    {
 
     }
 
-    public function postMultiPartMixed($url, $file, $json) {
+    public function postMultiPartMixed($url, $file, $json)
+    {
 
     }
 
-    public function postMultiPartFormData($url, $file, $formData) {
+    public function postMultiPartFormData($url, $file, $formData)
+    {
 
     }
 
-    function setProtocol($protocol) {
+    public function setProtocol($protocol)
+    {
         $this->protocol = $protocol;
     }
 
-    function setHost($host) {
+    public function setHost($host)
+    {
         $this->host = $host;
     }
 
-    function setPort($port) {
+    public function setPort($port)
+    {
         $this->port = $port;
     }
 
