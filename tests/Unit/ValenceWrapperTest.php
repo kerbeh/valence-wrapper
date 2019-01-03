21d1eebefd18470bf1d9805db835c8bfb66c0969
Tristan Kirby <t.kirby@deakin.edu.au>
2019-01-03 16:33:28 +1100

    ✅ Added more model tests

--- tests/Unit/ValenceWrapperTest.php
+++ tests/Unit/ValenceWrapperTest.php
@@ -6,6 +6,11 @@ use PHPUnit\Framework\TestCase;
 use ValenceWrapper\GuzzleAdapter;
 use ValenceWrapper\ValenceApi;
 use ValenceWrapper\Model\Grade\IncomingGradeValueNumeric;
+use ValenceWrapper\Model\Basic\UtcDateTime;
+use ValenceWrapper\Model\Grade\GradeObjectCategoryData;
+use DateTime;
+use ValenceWrapper\Model\Grade\GradeObjectNumeric;
+use ValenceWrapper\Model\Grade\GradeValue;
 
 class ValenceWrapperTest extends TestCase {
 
@@ -52,8 +57,107 @@ class ValenceWrapperTest extends TestCase {
         $incomingGradeValueNumeric = new IncomingGradeValueNumeric($incomingGradeValueNumericArray);
         $this->assertInstanceOf(IncomingGradeValueNumeric::class, $incomingGradeValueNumeric);
 
-        $this->assertEquals(10, $incomingGradeValueNumeric->getPointsNumerator());
+        $this->assertEquals($pointsNumerator, $incomingGradeValueNumeric->getPointsNumerator());
         $this->assertEquals(1, $incomingGradeValueNumeric->getGradeObjectType());
+    }
+
+    /** @test */
+    public function testUtcDateTime() {
+
+        $testDate = "2046-05-20T13:05:30.067Z";
+        $currentDate = new DateTime();
+
+        $testCurrentDate = new UtcDateTime($currentDate->format("Y-m-d H:i:s"));
+        $utcDateTime = new UtcDateTime($testDate);
+
+        $this->assertRegExp('/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/', $testCurrentDate->getUtcDateTime());
+        $this->assertEquals($testDate, $utcDateTime->getUtcDateTime());
+    }
+
+    /** @test */
+    public function testGradeObjectCategoryData() {
+        $currentDate = new DateTime();
+
+
+        $gradeObjectCategoryDateTestArray = [
+            "Name" => "Test Name",
+            "ShortName" => "Test Short Name",
+            "CanExceedMax" => FALSE,
+            "ExcludeFromFinalGrade" => TRUE,
+            "StartDate" => "2019-01-03T03:23:19.000Z",
+            "EndDate" => "2019-01-03T03:23:19.000Z",
+            "Weight" => NULL,
+            "MaxPoints" => 100,
+            "AutoPoints" => NULL,
+            "WeightDistributionType" => NULL,
+            "NumberOfHighestToDrop" => NULL,
+            "NumberOfLowestToDrop" => NULL,
+        ];
+
+        $gradeObjectCategoryDataTestObject = new GradeObjectCategoryData($gradeObjectCategoryDateTestArray);
+        $this->assertJsonStringEqualsJsonString($gradeObjectCategoryDataTestObject->toJson(), '{"Name":"Test Name","ShortName":"Test Short Name","CanExceedMax":true,"ExcludeFromFinalGrade":true,"StartDate":"2019-01-03T03:23:19.000Z","EndDate":"2019-01-03T03:23:19.000Z","Weight":null,"MaxPoints":100,"AutoPoints":null,"WeightDistributionType":null,"NumberOfHighestToDrop":null,"NumberOfLowestToDrop":null}');
+        $this->assertInstanceOf(GradeObjectCategoryData::class, $gradeObjectCategoryDataTestObject);
+    }
+
+    public function testGradeObjectNumeric() {
+
+        $gradeObjectNumericTestArray = [
+            "MaxPoints" => 100,
+            "CanExceedMaxPoints" => FALSE,
+            "IsBonus" => FALSE,
+            "ExcludeFromFinalGradeCalculation" => TRUE,
+            "GradeSchemeId" => NULL,
+            "Name" => "Test Grade Object",
+            "ShortName" => "Short Name Test Grade Object",
+            "CategoryId" => NULL,
+            "Description" => NULL,
+            "Weight" => 10,
+            "AssociatedTool" => NULL
+        ];
+
+        $GradeObjectNumericTestObject = new GradeObjectNumeric($gradeObjectNumericTestArray);
+
+        $this->assertJsonStringEqualsJsonString($GradeObjectNumericTestObject->toJson(), '{"MaxPoints": 100,"CanExceedMaxPoints": true,"IsBonus": false,"ExcludeFromFinalGradeCalculation": true,"GradeSchemeId": null,"Id": null,"Name": "Test Grade Object","ShortName": "Short Name Test Grade Object","GradeType": "Numeric","CategoryId": 0,"Description": null,"Weight": 10,"ActivityId": null,"AssociatedTool": null}');
+        $this->assertInstanceOf(GradeObjectNumeric::class, $GradeObjectNumericTestObject);
+    }
+
+    public function testGradeValue() {
+
+        $gradeValueTestArray = [
+            "UserId" => "",
+            "OrgUnitId" => "",
+            "DisplayedGrade" => "10 %",
+            "GradeObjectIdentififier" => "236276",
+            "GradeObjectName" => "Test Grade Object",
+            "GradeObjectType" => "1",
+            "GradeObjectTypeName" => "Numeric",
+            "Comments" => Null,
+            "PrivateComments" => Null,
+            "PointsNumerator" => 10,
+            "PointsDenominator" => 100,
+            "WeightedDenominator" => 10,
+            "WeightedNumerator" => 1
+        ];
+        $gradeValueTestObject = new GradeValue($gradeValueTestArray);
+
+
+        $this->assertInstanceOf(GradeValue::class, $gradeValueTestObject);
+
+        $this->assertJsonStringEqualsJsonString($gradeValueTestObject->toJson(), '{"UserId":null,"OrgUnitId":null,"DisplayedGrade":"10 %","GradeObjectIdentififier":"236276","GradeObjectName":"Test Grade Object","GradeObjectType":"1","GradeObjectTypeName":"Numeric","Comments":null,"PrivateComments":null,"PointsNumerator":10,"PointsDenominator":100,"WeightedDenominator":10,"WeightedNumerator":1}');
+    }
+
+    public function testIncomingGradeItemNumeric() {
+
+        $testIncomingGradeItemNumericTestArray = [
+            'Comments' => Null,
+            'PrivateComments' => Null,
+            'PointsNumerator' => 10
+        ];
+
+        $incomingGradeValueNumericTestObject = new IncomingGradeValueNumeric($testIncomingGradeItemNumericTestArray);
+
+        $this->assertJsonStringEqualsJsonString($incomingGradeValueNumericTestObject->toJson(), '{"GradeObjectType":1, "PointsNumerator":10, "Comments":null, "PrivateComments":null}');
+        $this->assertInstanceOf(IncomingGradeValueNumeric::class, $incomingGradeValueNumericTestObject);
     }
 
 }
