f38bd049c287b6a71016dc2096efd90497987dd2
Tristan Kirby <t.kirby@deakin.edu.au>
2019-01-07 10:18:35 +1100

    🔨 Added retrieve grades functionality.

--- src/Service/Grades.php
+++ src/Service/Grades.php
@@ -10,6 +10,7 @@ namespace ValenceWrapper\Service;
 
 use ValenceWrapper\Model\Grade\GradeValue;
 use ValenceWrapper\ValenceInstance;
+use ValenceWrapper\Model\User\User;
 
 /**
  * Description of Grades
@@ -44,16 +45,37 @@ class Grades {
     }
 
     /**
+     * Get a paginated and filtered list of grades for the specified grade object.
      * @url https://docs.valence.desire2learn.com/res/grade.html?highlight=displayedgrade#get--d2l-api-le-(version)-(orgUnitId)-grades-(gradeObjectId)-values-
      * @param type $orgUnitId
      * @param type $gradeObjectId
-     * @return string
+     * @param String $sort Optional. Sort by firstname, lastname, or grade value (see description).
+     * @param Int $pageSize Pagination size defautl and max are 200
+     * @param Bool $isGraded If true only return users with a non null grade.
+     * @param String $searchText String to search the first and last name fields
+     * @return type
      */
-    public function getGrades($orgUnitId, $gradeObjectId) {
+    public function getGrades($orgUnitId, $gradeObjectId, $searchText = "", $sort = "lastname", $pageSize = 200, $isGraded = true) {
         $urlStem = "/d2l/api/le/$this->le_version/$orgUnitId/grades/$gradeObjectId/values/";
+        $urlQuery = http_build_query([
+            "searchText" => $searchText,
+            "sort" => $sort,
+            "pageSize" => $pageSize,
+            "isGrade" => $isGraded
+        ]);
 
-        $apiResponse = $this->httpClient->get($this->valenceInstance->authenticateUri($urlStem, "GET"));
-        return $apiResponse->getJsonResponse();
+        $apiResponse = $this->httpClient->get($this->valenceInstance->authenticateUri("$urlStem?$urlQuery", "GET"));
+
+
+
+        $compositeUserGradesArray = array_map(function($userGrade) {
+            return [
+                new User($userGrade["User"]),
+                new GradeValue($userGrade["GradeValue"])
+            ];
+        }, $apiResponse->getJsonResponse()["Objects"]);
+
+        return $compositeUserGradesArray;
     }
 
 }
--- tests/Unit/ServiceTest/GradesTest.php
+++ tests/Unit/ServiceTest/GradesTest.php
@@ -3,14 +3,16 @@
 namespace Tests\Unit\ServiceTests;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Constraint\IsType;
 use ValenceWrapper\Service\Grades;
 use ValenceWrapper\GuzzleAdapter;
 use ValenceWrapper\ValenceInstance;
+use ValenceWrapper\Model\Grade\GradeValue;
 
 class GradesTests extends TestCase {
 
     /** @test */
-    public function testGradeService() {
+    public function GetIndividualGrade() {
         /**
          * Prepare a HTTP Client
          */
@@ -28,13 +30,51 @@ class GradesTests extends TestCase {
         $port = 443;
         $protocol = "HTTPS";
 
+
         $valenceInstance = new ValenceInstance($httpClient, $userId, $userKey, $appId, $appKey, $host, $port, $protocol);
 
 
+        $userId = "237486";
+        $orgUnitId = "376743";
+        $gradeObjectId = "156716";
+
         $gradeService = new Grades($valenceInstance);
 
-        var_dump($gradeService->getGrades(376743, 156716));
+        $gradeResponse = $gradeService->getUserGrade($orgUnitId, $gradeObjectId, $userId);
 
+        $this->assertInstanceOf(GradeValue::class, $gradeResponse);
+        $this->assertInstanceOf(Grades::class, $gradeService);
+    }
+
+    public function testGetAllGradesForGradeObject() {
+        /**
+         * Prepare a HTTP Client
+         */
+        $httpClient = new GuzzleAdapter;
+
+        /**
+         * Create an instance of the api wrapper
+         */
+        $userId = "1";
+        $userKey = "userKey";
+        $appId = "G9nUpvbZQyiPrk3um2YAkQ";
+        $appKey = "ybZu7fm_JKJTFwKEHfoZ7Q";
+
+        $host = "lms.valence.desire2learn.com";
+        $port = 443;
+        $protocol = "HTTPS";
+
+
+        $valenceInstance = new ValenceInstance($httpClient, $userId, $userKey, $appId, $appKey, $host, $port, $protocol);
+
+
+        $userId = "237486";
+        $orgUnitId = "376743";
+        $gradeObjectId = "156716";
+
+        $gradeService = new Grades($valenceInstance);
+        $gradeResponse = $gradeService->getGrades($orgUnitId, $gradeObjectId);
+        $this->assertInternalType(IsType::TYPE_ARRAY, $gradeResponse);
         $this->assertInstanceOf(Grades::class, $gradeService);
     }
 
